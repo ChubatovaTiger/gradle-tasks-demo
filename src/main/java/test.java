fa002fb57da352dc70c1e5b22bbd3723fc97ce15
tejkumar51 <tejkumar51@gmail.com>
2016-07-13 17:11:18 +0530

    CI-000 : Adding test code for date

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,14 @@
 package src.main.java;
+import java.util.Date;
 public class test {
-
+private Date date = null;
 	public static void main(String[] args) {
 		System.out.println("*********** test class called ********");
+	
 	}
+	
+	  public void doSomething() {
+        date = new Date();
+    }
 
 }
